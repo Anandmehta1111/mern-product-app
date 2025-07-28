84859ecc3d8533b6a9eec97ed95733b070d94599
Anand kumar <anandmahto1820@gmail.com>
2025-07-28 10:05:44 +0530

    Create server.js

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+const express = require('express'); 
+const mongoose = require('mongoose'); 
+const cors = require('cors'); 
+const productRoutes = require('./routes/productRoutes'); 
+const app = express(); 
+app.use(cors()); 
+app.use(express.json()); 
+mongoose.connect('mongodb://127.0.0.1:27017/productsDB', { 
+useNewUrlParser: true, 
+useUnifiedTopology: true 
+}) 
+.then(() => console.log('MongoDB connected')) 
+.catch((err) => console.error(err)); 
+app.use('/api/products', productRoutes); 
+app.listen(4000, () => { 
+console.log('Server running on http://localhost:4000'); 
+});
