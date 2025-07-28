1fde8f82bd70e645ea8631199ac1c54325b32b72
Anand kumar <anandmahto1820@gmail.com>
2025-07-28 10:10:42 +0530

    Create productRoutes.js

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,13 @@
+const express = require('express'); 
+const router = express.Router(); 
+const Product = require('../models/Product'); 
+router.post('/', async (req, res) => { 
+const newProduct = new Product(req.body); 
+const savedProduct = await newProduct.save(); 
+res.status(201).json(savedProduct); 
+}); 
+router.get('/', async (req, res) => { 
+const products = await Product.find(); 
+res.json(products); 
+}); 
+module.exports = router;
